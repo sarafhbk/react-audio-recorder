a91722dc6aac89b8cf9b10b0d8a852fb327f1c37
sarafhbk <saraffath@gmail.com>
2021-07-04 18:31:12 +0530

    common hook function for react recorder package

--- src/react-audio-recorder/ReactAudioRecorder.tsx
+++ src/react-audio-recorder/ReactAudioRecorder.tsx
@@ -1,15 +1,5 @@
 import { ReactElement } from 'react'
-import { useRef, useState } from 'react'
-import { RECORD_STATUS } from './status'
-import useTimer from './useTimer'
-
-type record_status_type =
-  | RECORD_STATUS.RECORDING
-  | RECORD_STATUS.PAUSED
-  | RECORD_STATUS.IDLE
-
-let mediaRecorder: MediaRecorder
-let localStream: MediaStream
+import { useAudioRecorder } from './useAudioRecorder'
 
 export const ReactAudioRecorder = ({
   render
@@ -34,90 +24,16 @@ export const ReactAudioRecorder = ({
     timer: number
   }) => ReactElement
 }) => {
-  const dataArray = useRef<Array<Blob>>([])
-
-  const [status, setStatus] = useState<record_status_type>(RECORD_STATUS.IDLE)
-  const [audioResult, setAudioResult] = useState<string>('')
-  const [errorMessage, setErrorMessage] = useState<string>('')
-
   const {
-    timer,
-    handleStartTimer,
-    handlePauseTimer,
-    handleResumeTimer,
-    handleResetTimer
-  } = useTimer()
-
-  const startRecording = () => {
-    if (status === RECORD_STATUS.IDLE) {
-      try {
-        setErrorMessage('')
-        navigator.mediaDevices
-          .getUserMedia({ audio: true })
-          .then((mediaStreamObj: MediaStream) => {
-            localStream = mediaStreamObj
-            mediaRecorder = new MediaRecorder(mediaStreamObj)
-            mediaRecorder.start()
-            mediaRecorder.onstart = () => {
-              handleStartTimer()
-              setStatus(RECORD_STATUS.RECORDING)
-            }
-            mediaRecorder.ondataavailable = (event: BlobEvent) => {
-              dataArray.current.push(event.data)
-            }
-          })
-          .catch((error) => {
-            setErrorMessage(error?.message)
-          })
-      } catch (error) {
-        setErrorMessage(error?.message)
-      }
-    } else {
-      return
-    }
-  }
-
-  const resumeRecording = () => {
-    if (status === RECORD_STATUS.PAUSED) {
-      mediaRecorder.resume()
-      mediaRecorder.onresume = () => {
-        handleResumeTimer()
-        setStatus(RECORD_STATUS.RECORDING)
-      }
-    } else {
-      return
-    }
-  }
-
-  const pauseRecording = () => {
-    if (status === RECORD_STATUS.RECORDING) {
-      mediaRecorder.pause()
-      mediaRecorder.onpause = () => {
-        handlePauseTimer()
-        setStatus(RECORD_STATUS.PAUSED)
-      }
-    } else {
-      return
-    }
-  }
-
-  const stopRecording = () => {
-    if (status !== RECORD_STATUS.IDLE) {
-      mediaRecorder.stop()
-      mediaRecorder.onstop = () => {
-        handleResetTimer()
-        let audioData = new Blob(dataArray.current, { type: 'audio/wav;' })
-        dataArray.current = []
-        setAudioResult(window.URL.createObjectURL(audioData))
-        setStatus(RECORD_STATUS.IDLE)
-        localStream.getAudioTracks().forEach((track: MediaStreamTrack) => {
-          track.stop()
-        })
-      }
-    } else {
-      return
-    }
-  }
+    startRecording,
+    stopRecording,
+    pauseRecording,
+    resumeRecording,
+    status,
+    audioResult,
+    errorMessage,
+    timer
+  } = useAudioRecorder()
 
   return render({
     startRecording,
