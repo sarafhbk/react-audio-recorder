38a8a677d61e1e6b174ff6cfddee4a7d5798e08c
sarafhbk <saraffath@gmail.com>
2021-07-04 17:58:41 +0530

    Updates

---
+++ precache-manifest.02806ca9991d2fa6758129a8e6f53cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce462a552fb190000e157994e10a154d",
+    "url": "/react-audio-recorder/index.html"
+  },
+  {
+    "revision": "0a82fd4f427f6cf2a538",
+    "url": "/react-audio-recorder/static/css/main.fc93aec4.chunk.css"
+  },
+  {
+    "revision": "a3efa9e3375898597bfc",
+    "url": "/react-audio-recorder/static/js/2.47d29443.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-audio-recorder/static/js/2.47d29443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a82fd4f427f6cf2a538",
+    "url": "/react-audio-recorder/static/js/main.fb5a45d4.chunk.js"
+  },
+  {
+    "revision": "3440d7d8229e605b593b",
+    "url": "/react-audio-recorder/static/js/runtime-main.e2944ee6.js"
+  }
+]);
